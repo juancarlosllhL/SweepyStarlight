104600bed0f0dcfd601ceefe95e76ba82b12b171
Juan Carlos Lloret <juan.carlos.lloret@lansweeper.com>
2023-11-13 12:59:57 +0100

    🔥 removed unnecessary dependencies

--- examples/basics/astro.config.mjs
+++ examples/basics/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from '@lansweeper/sweepy-starlight';
 
 // https://astro.build/config
 export default defineConfig({
